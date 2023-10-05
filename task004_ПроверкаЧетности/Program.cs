944572174df0fc20dd14f14f3cd4756523b6c464
tima <timoshaa96@gmail.com>
2023-10-05 18:48:32 +0300

    Add task004

---
+++ task004_ПроверкаЧетности/Program.cs
@@ -0,0 +1,11 @@
+﻿static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+if(number%2==0){
+  System.Console.WriteLine("Число `"+number+ "` чётное");
+}else{
+   System.Console.WriteLine("Число `"+number+ "` нечётное");
+}
+    }
+     int number=6;
+     CheckIfEven(number);
