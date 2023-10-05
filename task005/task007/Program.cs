20f303fa140d048f758ceb40956c668d404d6772
tima <timoshaa96@gmail.com>
2023-10-05 18:04:59 +0300

    add folders

---
+++ task005/task007/Program.cs
@@ -0,0 +1,6 @@
+﻿//7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+
+System.Console.WriteLine("Enter yout number");// общаемся с человеком
+int userNumber =Convert.ToInt32(Console.ReadLine());// запрашиваем число
+System.Console.WriteLine(userNumber%10);
