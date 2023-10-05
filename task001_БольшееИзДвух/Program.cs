f2cf66d966cfbc668ead0ef7b013cee244e2b587
tima <timoshaa96@gmail.com>
2023-10-05 18:31:22 +0300

    add task001

---
+++ task001_БольшееИзДвух/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int firstNumber, secondNumber;
+firstNumber = -3;
+secondNumber = -9;
+if (firstNumber < secondNumber)
+{
+    System.Console.WriteLine("Первое число " + firstNumber + " меньше чем второе число " + secondNumber);
+}
+else if (firstNumber == secondNumber)
+{
+    System.Console.WriteLine("Введенные числа равны " + firstNumber);
+}
+else
+{
+    System.Console.WriteLine("Первое число " + firstNumber + " больше чем второе число " + secondNumber);
+
+}
+
