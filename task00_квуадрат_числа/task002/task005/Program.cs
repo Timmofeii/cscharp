32dc0984c579f83c9d9a36848086ac1aa4f47662
tima <timoshaa96@gmail.com>
2023-10-05 12:42:56 +0300

    add tasks

---
+++ task00_квуадрат_числа/task002/task005/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata;
+System.Console.WriteLine("Enter yout number");// общаемся с человеком
+int userNumber =Convert.ToInt32(Console.ReadLine());// запрашиваем число
+for(int i= 0-userNumber;i<= userNumber;i++){// шагаем в цикле до нашего числа 
+  System.Console.Write(i+" ");// выводим в консоль значение i(0, 1, 2..numberUser)
+   // if(i%2==0){//выводим все парные 
+      //   System.Console.Write(i+" ");
+    //}
+}
