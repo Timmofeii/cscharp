bf321e7bb6eec78f12573e4c70567a62ad9ca867
tima <timoshaa96@gmail.com>
2023-10-10 22:44:35 +0300

    Добавил task009

---
+++ task006_квадратЧисла1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Возвести число в квадрат 
+Console.Clear();
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
