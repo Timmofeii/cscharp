c7b62ac748e1a619deca0e2660dd4ad62c324bd8
tima <timoshaa96@gmail.com>
2023-10-10 18:18:47 +0300

    добавили условия в task005  и поменяли чуток решение

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,11 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
 
 using System.Reflection.Metadata;
 System.Console.WriteLine("Enter yout number");// общаемся с человеком
-int userNumber =Convert.ToInt32(Console.ReadLine());// запрашиваем число
-for(int i= 0-userNumber;i<= userNumber;i++){// шагаем в цикле до нашего числа 
-  System.Console.Write(i+" ");// выводим в консоль значение i(0, 1, 2..numberUser)
-   // if(i%2==0){//выводим все парные 
-      //   System.Console.Write(i+" ");
-    //}
+int userNumber = Convert.ToInt32(Console.ReadLine());// запрашиваем число
+for (int i = -userNumber; i <= userNumber; i++)
+{// шагаем в цикле до нашего числа 
+  System.Console.Write(i + " ");// выводим в консоль значение i(-usernumber...0, 1, 2..numberUser)
+                                // if(i%2==0){//выводим все парные 
+                                //   System.Console.Write(i+" ");
+                                //}
 }
