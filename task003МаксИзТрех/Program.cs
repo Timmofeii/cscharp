3630e79934a546133ab4536b7508dad7a0062eca
tima <timoshaa96@gmail.com>
2023-10-10 18:14:09 +0300

    Добавил условия в task004

--- task003МаксИзТрех/Program.cs
+++ task003МаксИзТрех/Program.cs
@@ -1,19 +1,30 @@
-﻿  int a, b, c;
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int a, b, c;
 
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
-           a = 5;
-           b = 8;
-           c = 7;
-           static int FindMax(int a, int b, int c)
-    {
-     if(a>b&&a>c){
-     return a;
-     }else if (b>a&&b>c){
-       return b;
-       
-     }else{
-       return c;
-     }
-    }
-         int result = FindMax(a, b, c);
-        System.Console.WriteLine($"{result}");
+// Здесь вы можете поменять значения для отправки кода на Выполнение
+a = 5;
+b = 8;
+c = 7;
+static int FindMax(int a, int b, int c)
+{
+  if (a > b && a > c)
+  {
+    return a;
+  }
+  else if (b > a && b > c)
+  {
+    return b;
+
+  }
+  else
+  {
+    return c;
+  }
+}
+int result = FindMax(a, b, c);
+System.Console.WriteLine($"{result}");
--- task004_ПроверкаЧетности/Program.cs
+++ task004_ПроверкаЧетности/Program.cs
@@ -1,11 +1,22 @@
-﻿static void CheckIfEven(int number)
-    {
-      // Введите свое решение ниже
-if(number%2==0){
-  System.Console.WriteLine("Число `"+number+ "` чётное");
-}else{
-   System.Console.WriteLine("Число `"+number+ "` нечётное");
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+static void CheckIfEven(int number)
+{
+  // Введите свое решение ниже
+  if (number % 2 == 0)
+  {
+    System.Console.WriteLine("Число `" + number + "` чётное");
+  }
+  else
+  {
+    System.Console.WriteLine("Число `" + number + "` нечётное");
+  }
 }
-    }
-     int number=6;
-     CheckIfEven(number);
+//int number=6;
+System.Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CheckIfEven(number);
