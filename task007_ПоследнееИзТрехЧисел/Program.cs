738057346fbdb74a28492798b8a88ef031514ab2
tima <timoshaa96@gmail.com>
2023-10-10 18:37:31 +0300

    Добавил папки обратно...

---
+++ task007_ПоследнееИзТрехЧисел/Program.cs
@@ -0,0 +1,9 @@
+﻿//7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+int userNumber = 0;
+while (userNumber < 100 || userNumber > 999)
+{
+    System.Console.WriteLine("Enter your number");// общаемся с человеком
+    userNumber = Convert.ToInt32(Console.ReadLine());// запрашиваем число
+}
+System.Console.WriteLine(userNumber % 10);
