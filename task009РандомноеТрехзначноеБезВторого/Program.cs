5030dbd655bad9b470d0e55f87699f903ce49d61
tima <timoshaa96@gmail.com>
2023-10-10 22:58:55 +0300

    Add task009, task010

---
+++ task009РандомноеТрехзначноеБезВторого/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿/*
+Написать программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int random = new Random().Next(100,1000);
+int withoutSecond= (random/100)*100+ (random%10);
+System.Console.WriteLine(random);
+System.Console.WriteLine(withoutSecond);
---
+++ task010ВтороеИзТрех/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int randome = new Random().Next(100,1000);
+System.Console.WriteLine(randome);
+randome= (randome%100-randome%10)/10;
+
+System.Console.WriteLine(randome);
