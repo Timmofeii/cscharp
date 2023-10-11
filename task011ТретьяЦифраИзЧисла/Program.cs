6d161282917bc71d5af69b47b2016342f59e1654
tima <timoshaa96@gmail.com>
2023-10-11 16:06:28 +0300

    Подправил task011

---
+++ task011ТретьяЦифраИзЧисла/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+class Program
+{
+  static void Main()
+  {
+    int userNumber = GetValidNumber();
+    int thirdDigit = GetThirdDigit(userNumber);
+
+    if (thirdDigit != -1)
+    {
+      Console.WriteLine(thirdDigit);
+    }
+    else
+    {
+      Console.WriteLine("Третьей цифры нет");
+    }
+  }
+  static int GetValidNumber()
+  {
+    bool validInput = false;
+    int number = 0;
+    Console.WriteLine("Введите ваше число:");
+    while (!validInput)
+    {
+      if (int.TryParse(Console.ReadLine(), out number))
+      {
+
+        validInput = true;
+      }
+      else
+      {
+        System.Console.WriteLine("Неверный символ. Пожалуйста, введите число.");
+      }
+    }
+    return number;
+  }
+  static int GetThirdDigit(int number)
+  {
+    string numberString = number.ToString();
+    const int ThirdDigitIndex = 2;
+    if (numberString.Length >= 3)
+    {
+      return int.Parse(numberString[2].ToString());
+    }
+    return -1;//означает что третьей цифры нет.
+  }
+
+}
---
+++ task012ВыходнойЛи/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int dayOfWeek;
+const int minDay = 1;
+const int maxDay = 7;
+bool validInput = false;
+Console.WriteLine("Введите номер дня недели:");
+while (!validInput)
+{
+    if (int.TryParse(Console.ReadLine(), out dayOfWeek) && dayOfWeek >= minDay && dayOfWeek <= maxDay)
+    {
+        validInput = truel
+}
+    else { System.Console.WriteLine("Неверное число. Пожалуйста, введите число от 1 до 7."); }
+
+}
+if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+    Console.WriteLine("Да, это выходной день.");
+}
+else
+{
+    Console.WriteLine("Нет, это не выходной день.");
+}
