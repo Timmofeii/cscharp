6f2cc2af50984b9f20ee4bc4101ef2c7155b70b9
tima <timoshaa96@gmail.com>
2023-10-11 17:04:29 +0300

    Добавил task013

---
+++ task013_КратноеДвумЧислам/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> 
+*/
+class Program
+{
+    static void Main()
+    {
+       System.Console.WriteLine(multipleOfTwo()); 
+    }
+    static string multipleOfTwo(){
+        System.Console.WriteLine("Enter your");
+        int firstNUmber = Convert.ToInt32(Console.ReadLine());
+        if(firstNUmber%7==0&&firstNUmber%23==0){
+          return "да";
+        }
+        else{
+           return "нет";
+        }
+         
+
+    }
+}
