da1234fc2f384c6c8992fd15bf1afe43c71749fa
Timm Bolshakov <94001232+Timmofeii@users.noreply.github.com>
2023-10-12 23:08:22 +0300

    Update Program.cs

--- task019_Палиндром/Program.cs
+++ task019_Палиндром/Program.cs
@@ -17,7 +17,7 @@ class Program019
   {
     int number = 234322;
     bool isPalindrome = IsPalindrome(number);// первое решение к которому я сам пришел и как захотел решать 
-    bool isPalindrome2 = IsPalindromeSecond(number);// решение которое сам написл после просмотра код коллег
+    bool isPalindrome2 = IsPalindromeSecond(number);// решение которое сам написл после просмотра подхода коллег)
     Console.WriteLine(isPalindrome);
     Console.WriteLine(isPalindrome);
   }
