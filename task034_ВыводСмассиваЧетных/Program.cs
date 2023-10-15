3afcad044333e1cdbc254814443424ee078c2f0d
tima <timoshaa96@gmail.com>
2023-10-15 21:04:43 +0300

    Добавил task034

--- task034_ВыводСмассиваЧетных/Program.cs
+++ task034_ВыводСмассиваЧетных/Program.cs
@@ -1,3 +1,32 @@
 ﻿﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
+class Program034{
+    public static void Main(string []args){
+      int [] arr= new int[8];
+       FullArrRandomNumbers(arr);
+      
+     
+     System.Console.WriteLine( HowManyEvenInArray(arr));
+    } 
+    private static void FullArrRandomNumbers(int []arr){
+ var rand = new Random();
+      
+    
+     for (int i = 0; i < arr.Length; i++)
+     {
+        arr[i] = rand.Next(100,1000);
+     }
+    }
+     private static int HowManyEvenInArray(int []arr){
+         int count=0;  for (int i = 0; i < arr.Length; i++)
+     
+        {
+            if(arr[i]%2==0){
+                count++;
+            }
+        
+        }
+     return count;}
+    
+}
