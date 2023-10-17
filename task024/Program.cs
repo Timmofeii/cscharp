cb6c7143b8b63524adaddef7473fac5ead311773
tima <timoshaa96@gmail.com>
2023-10-17 10:45:47 +0300

    Поправил имена tasks и дописал 24-е с классной работы

--- task024/Program.cs
+++ task024/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Xml.XPath;
+
+class Program024{
+    public static void Main(string[]args){
+        System.Console.WriteLine("Enter number");
+        int userNumber=Convert.ToInt32(Console.ReadLine());
+        int sum=Sum(userNumber);
+        System.Console.WriteLine(sum);
+    }
+    private static int Sum(int userNumber){
+        int result=0;
+        for (int i = 1; i <= userNumber; i++)       {
+           result+=i; 
+        }
+        return result;
+    }
+}
---
+++ task026_СчетЦифрВЧисле/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{// должен считать цифрЫ
+    public static void Main(string[] args)
+    {
+        int number = 123456; 
+        int countDg=CountDg(number);
+      System.Console.WriteLine(countDg); 
+    }
+    private static int CountDg(int number)
+    {
+        string stringNumber= number.ToString();
+        
+       
+      
+ 
+        return  stringNumber.Length;
+    }
+}
