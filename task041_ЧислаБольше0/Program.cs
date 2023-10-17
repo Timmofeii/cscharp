2af734a93542643be14c4f0bda79c52bd4116e97
tima <timoshaa96@gmail.com>
2023-10-17 22:04:59 +0300

    added task42

--- task041_ЧислаБольше0/Program.cs
+++ task041_ЧислаБольше0/Program.cs
@@ -32,4 +32,3 @@ System.Console.WriteLine(count);
         }
         return arr;
     }
-}
---
+++ task042ДесятичноеВДвоичное/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+using System;
+using System.Text;
+public class Program042
+{
+    public static void Main(string[] args)
+    {
+        int decimenalNumber = 18;
+      string binryString = DecimalToBinary(decimenalNumber);
+    
+    System.Console.Write(binryString);
+      
+    }
+    private static string  DecimalToBinary(int number){
+        if(number==0){
+            return"0";
+        }
+
+        StringBuilder binary= new StringBuilder();
+        
+   
+
+   while(number>0)
+    {
+     int remainder=number%2; 
+     binary.Insert(0,remainder);//вставляем каждый новый элемент в начало списка, так не нужно будет переставлять последний элемент на первый и тд...
+      number/=2;
+
+        
+    }
+
+    return binary.ToString();}
+    
+}
