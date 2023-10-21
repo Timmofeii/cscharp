3ce7bb326c3816573dc0cbe4a4926291ed4b1e3d
tima <timoshaa96@gmail.com>
2023-10-21 19:13:50 +0300

    Пытаюсь все перятнуть в visual studio

--- task048_ДвухмерыйМассивЗначениеСуммаЕгоИндексов/Program.cs
+++ task048_ДвухмерыйМассивЗначениеСуммаЕгоИндексов/Program.cs
@@ -4,23 +4,26 @@ class Program048// не уверен что правильно понял усл
 {
     public static void Main(string[] args)
     {
-        FullArrInSumHisIndex();
-    }
-    private static void FullArrInSumHisIndex()
-    {
+        int m = 3;//задайте размер m
+        int n = 3;//задайте размер n
         int[][] arr = new int[3][];
         for (int i = 0; i < arr.Length; i++)
         {
             arr[i] = new int[3];
-            System.Console.Write("[ ");
             for (int j = 0; j < arr[i].Length; j++)
             {
                 arr[i][j] = i + j;
-                System.Console.Write(arr[i][j]+" ");
-
             }
-            System.Console.WriteLine("]");
 
         }
+        PrintArr(arr);
     }
+    private static void PrintArr(int[][] arr)
+    {
+        foreach (var row in arr)
+        {
+            System.Console.WriteLine($"[{string.Join(" ", row)}]");
+        }
+    }
+
 }
