ff065e1808ea835e49775c899734c662aa8355a2
tima <timoshaa96@gmail.com>
2023-10-22 13:57:28 +0300

    Дописал task052

--- task051_СуммаЕлементовПоГлавнойДиагонали/Program.cs
+++ task051_СуммаЕлементовПоГлавнойДиагонали/Program.cs
@@ -1,7 +1,46 @@
-﻿﻿/*Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
+﻿/*Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
 (с индексами (0,0); (1;1) и т.д.*/
-class Program051 {
-    public static void Main(string[]args){
-        int [][]arr= new int[]
+class Program051
+{
+    public static void Main(string[] args)
+    {
+        int[][] arr = CreateAndFullArr(3, 3, 1, 10);
+        int sumDiagonal=FindSumDiagonal(arr);
+        Console.WriteLine(sumDiagonal);
+    }
+    private static int FindSumDiagonal(int[][] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                if (i == j) { sum += arr[i][j]; }
+
+
+            }
+        }
+        return sum;
+    }
+    public static int[][] CreateAndFullArr(int row, int col, int startArr, int endArr)
+    {
+        int[][] arr = new int[row][];
+        Random rd = new Random();
+        for (int i = 0; i < row; i++)
+        {
+            arr[i] = new int[col];
+            System.Console.Write("[");
+            for (int j = 0; j < arr.Length; j++)
+            {
+
+                arr[i][j] = rd.Next(startArr, endArr);
+                System.Console.Write(" " + arr[i][j]);
+
+            }
+            System.Console.WriteLine(" ]");
+
+
+        }
+        return arr;
     }
 }
---
+++ task052_AVG_ROW/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+public class Program052
+{
+    public static void Main(string[] args)
+    {
+        int[][] arr = new int[3][];
+        arr[0] = new int[] { 1, 4, 7, 2 };
+        arr[1] = new int[] { 5, 9, 2, 3 };
+        arr[2] = new int[] { 8, 4, 2, 4 };
+        AVGRowArray(arr);
+    }
+
+    public static void AVGRowArray(int[][] array)
+    {
+        global::System.Console.Write("Среднее арифметическое каждого столбца: ");
+        for (int i = 0; i < array[0].Length; i++)
+        {
+            double avgColumn = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+                avgColumn += array[j][i];
+            }
+            double result = Math.Round(avgColumn / array.Length, 2);
+            Console.Write(result);
+            if (i < array.Length)
+            {
+                global::System.Console.Write("; ");
+            }
+            else {
+                global::System.Console.Write(".");
+             }
+        }
+    }
+}
